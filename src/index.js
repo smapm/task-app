f09f3e7881804a6f032ad677548bfe6debc8f9ef
smapm <smsarath1234@gmail.com>
2020-04-12 00:31:40 +0530

    jest test scripts added

--- src/index.js
+++ src/index.js
@@ -1,29 +1,7 @@
-const express = require('express');
-require('./db/mongoose');
-const userRouter = require('./routers/user');
-const taskRouter = require('./routers/task');
-const app = express();
+const app = require('./app');
 
 const port = process.env.PORT;
 
-app.use(express.json());
-app.use(userRouter);
-app.use(taskRouter);
-
 app.listen(port, ()=>{
     console.log('server running on ' + port);  
-})
-
-// const Task = require('./models/task');
-// const User = require('./models/user');
-
-
-// (async()=>{
-//     // const task = await Task.findById('5e8b6982c359903b2415d696');
-//     // await task.populate('owner').execPopulate();
-//     // console.log(task);
-//     // const user = await User.findById('5e8b6961c359903b2415d694');
-//     // await user.populate('tasks').execPopulate();
-//     // console.log(user.tasks);
-    
-// })();
+});
---
+++ tests/user.test.js
@@ -0,0 +1,109 @@
+const request = require('supertest');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+const app = require('../src/app');
+const User = require('../src/models/user');
+
+const userOneId = new mongoose.Types.ObjectId();
+
+const userOne = {
+    _id: userOneId,
+    name: 'lilly',
+    email: 'lilly@example.com',
+    password: 'enieminemoe',
+    tokens: [{ token: jwt.sign({_id: userOneId}, process.env.JWT_SECRET)
+    }]
+};
+
+beforeEach(async()=>{
+    await User.deleteMany();
+    await new User(userOne).save();
+});
+
+test('sign up user', async()=>{
+    await request(app).post('/users').send({
+        name: "sarath",
+        email: "smsarath1234@gmail.com",
+        password: "qwertyuiop"
+    }).expect(201);
+});
+
+test('login user', async()=>{
+    const response = await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: userOne.password
+    }).expect(200);    
+    const user = await User.findById(response.body.user._id);
+    expect(user.tokens[1].token).toBe(response.body.token);
+});
+
+test('should not login with incorrect credentials', async()=>{
+    await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: 'fffffffff'
+    }).expect(400)
+});
+
+test('get user data with authentication', async()=>{
+    await request(app)
+    .get('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+});
+
+test('should not get user data without authorization', async()=>{
+    await request(app)
+    .get('/users/me')
+    .send()
+    .expect(401);
+});
+
+test('delete user with authorization', async()=>{
+    await request(app)
+    .delete('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+    const user = await User.findById(userOneId);
+    expect(user).toBeNull();
+});
+
+test('should not delete user without authorization', async()=>{
+    await request(app)
+    .delete('/users/me')
+    .send()
+    .expect(401);
+});
+
+test('should upload avatar', async()=>{
+    await request(app)
+    .post('/users/me/avatar')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .attach('avatar', 'tests/fixtures/profile-pic.jpg')
+    .expect(200);
+    const user = await User.findById(userOneId);
+    expect(user.avatar).toEqual(expect.any(Buffer));
+});
+
+test('should update user details', async()=>{
+    await request(app)
+    .patch('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send({
+        name: 'Bobby'
+    })
+    .expect(200);
+    const user = await User.findById(userOneId);
+    expect(user.name).toBe('Bobby')
+});
+
+test('should not update invalid user fields', async()=>{
+    await request(app)
+    .patch('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send({
+        location: 'mysore'
+    })
+    .expect(400);
+});
